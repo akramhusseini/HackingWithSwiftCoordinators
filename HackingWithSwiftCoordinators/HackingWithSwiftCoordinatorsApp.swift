881a3be063859e0a570f222bd71bf4b86811f775
akrammotory <93082320+akrammotory@users.noreply.github.com>
2024-07-22 16:27:15 +0300

    Initial Commit

---
+++ HackingWithSwiftCoordinators/HackingWithSwiftCoordinatorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackingWithSwiftCoordinatorsApp.swift
+//  HackingWithSwiftCoordinators
+//
+//  Created by Akram Husseini on 22/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct HackingWithSwiftCoordinatorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
